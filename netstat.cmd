c70fab4194f6ff50194a5d28ac1e5acdf640039e
Riverside Rocks <trent@riverside.rocks>
2020-07-28 09:22:43 -0400

    Make the time to wait longer

--- netstat.cmd
+++ netstat.cmd
@@ -1,5 +1,6 @@
 echo Active Connections
-timeout /t 1 /nobreak
+timeout /t 4 /nobreak
 echo Proto  Local Address          Foreign Address        State
-timeout /t 3 /nobreak
+timeout /t 6 /nobreak
 echo No remote connections found.
+timeout 10
